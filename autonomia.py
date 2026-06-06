cb26a14f7ac0fe554b490155ddd48f3b1a29f284
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-05 22:15:25 -0400

    Update autonomia.py

--- autonomia.py
+++ autonomia.py
@@ -1,5 +1,8 @@
 import memoria
+import datetime
 
 def ejecutar_ciclo():
-    memoria.registrar("AUTO_SYSTEM", "Autochequeo completado: Nodos sincronizados.")
-  
+    # Esta función simula que AMITI "piensa" y guarda algo
+    timestamp = datetime.datetime.now().strftime("%H:%M:%S")
+    memoria.registrar(f"PENSAMIENTO_{timestamp}", "Analizando entorno y optimizando nodos...")
+    
