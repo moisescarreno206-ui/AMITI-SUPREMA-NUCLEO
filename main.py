4953afdcd7370156c4f5c6c08c0efcef4a6d82ca
moisescarreno206-ui <moisescarreno206@gmail.com>
2026-06-05 21:01:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-from modulos import seguridad, autonomia, memoria
+import modulos.seguridad as seguridad
+import modulos.autonomia as autonomia
+import modulos.memoria as memoria
 
 app = Flask(__name__)
 
@@ -7,12 +9,12 @@ app = Flask(__name__)
 def index():
     if request.method == 'HEAD':
         return ""
-        
+    
     autonomia.ejecutar_ciclo()
     msg_salida = "AMITI NUCLEO SUPREMO: Estado activo"
     
     if request.method == 'POST':
-        # Verificamos seguridad antes de procesar
+        # Verificamos seguridad
         if seguridad.verificar(request.form.get("llave")):
             comando = request.form.get("msg")
             msg_salida = f"EJECUTADO: {comando}"
